2f818dfe37b203aae633adffc12560d39627c0bb
randrei12 <rantaandrei12@gmail.com>
2022-08-18 14:56:34 +0300

    #

--- www/db/patientHome-db.js
+++ www/db/patientHome-db.js
@@ -1,34 +1,15 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCC1TTaWpCU4bxhBD2l7ueviKKP1eskrpM",
-    authDomain: "medlog-9650b.firebaseapp.com",
-    databaseURL: "https://medlog-9650b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "medlog-9650b",
-    storageBucket: "medlog-9650b.appspot.com",
-    messagingSenderId: "955776050336",
-    appId: "1:955776050336:web:7b7e3a9cb38de68d3695c5",
-    measurementId: "G-424MJM444P"
-};
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-const database = firebase.firestore();
-const auth = firebase.auth();
 
 const cardsContainter = document.getElementById("cards-container");
 
-auth.onAuthStateChanged((user) => {
+firebase.auth().onAuthStateChanged(user => {
     if (user) {
         //blablabla
     }
-    else {
-        window.location = "./patSignUp.html";
-    }
+    else location = "./index.html";
 })
-// database.collection('users').where('type', '==', 'doctor').hrt()
-database.collection('users').get().then(snapshot => {
+// firebase.firestore().collection('users').where('type', '==', 'doctor').hrt()
+firebase.firestore().collection('users').get().then(snapshot => {
     snapshot.forEach(element => {
 
         console.log(element.id)
