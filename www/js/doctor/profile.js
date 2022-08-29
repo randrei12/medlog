bc849b1aa8b5f0c455dca0b6af241487ccea83fe
saracrisan05 <sara.crisan@gmail.com>
2022-08-29 11:37:25 +0300

    doc profile update

--- www/js/doctor/profile.js
+++ www/js/doctor/profile.js
@@ -1,18 +1,31 @@
 import '../../db/config.js';
-import { params } from "../Utils.js";
+import { params, roundTo } from "../Utils.js";
+import { getDoctorReview } from '../../db/DoctorService.js';
 import Loading from '../loading.js';
-let nameLabel = document.querySelector('.doctorName');
+const nameLabel = document.querySelector('.doctorName');
+const averageLabel = document.querySelector('.average');
+const specializationLabel = document.querySelector('.specialization');
+const phoneLabel = document.querySelector('.callButton');
+const emailLabel = document.querySelector('.emailButton');
+const doctorReviewURL = document.querySelector('#doctor')
 const loading = new Loading();
 loading.show()
-let uid = params.doctor
+const uid = params.doctor
 const userDataRef = firebase.firestore().collection("users").doc(uid);
 userDataRef
     .get()
-    .then((doc) => {
+    .then(async (doc) => {
         if (doc.exists) {
-            console.log("Document data:", doc.data());
+            const ratings = await getDoctorReview(uid);
+            console.log(ratings);
             const userData = doc.data();
-            nameLabel.innerText = 'Dr. ' + doc.data().username;
+            nameLabel.innerText = 'Dr. ' + userData.username;
+            averageLabel.innerText = `${roundTo(ratings.average)} rating`;
+            phoneLabel.href = `tel:${userData.phone}`;
+            emailLabel.href = `mailto:${userData.email}`;
+            console.log(specializationLabel);
+            specializationLabel.innerText = userData.specialization;
+            doctorReviewURL.href = `../../html/patient/reviewComponent.html?doctor=${uid}`
             loading.hide()
         } else {
             loading.hide();
