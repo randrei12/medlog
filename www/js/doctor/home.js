bb185c66fd3b74df76196c7c24cff60dd80e446f
Andrei Ranta <rantaandrei12@gmail.com>
2022-09-12 19:38:24 +0300

    minor fixes

--- www/js/doctor/home.js
+++ www/js/doctor/home.js
@@ -3,14 +3,10 @@ import { getLoggedUser } from '../Utils.js';
 
 const nameLabel = document.querySelector('.nameLabel');
 const loading = new Loading();
-if (!sessionStorage.getItem('user')) loading.show();
-else {
-    let data = JSON.parse(sessionStorage.getItem('user'));
-    nameLabel.innerText = data.firstName + ' ' + data.lastName;
-}
+loading.show();
 
 getLoggedUser().then(user => {
     if (!user) return location = "../../index.html";
-    nameLabel.innerText = data.firstName + ' ' + data.lastName;
+    nameLabel.innerText = user.firstName + ' ' + user.lastName;
     loading.hide();
 });
