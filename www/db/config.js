1a81ee2bfd36456da611662305e24fa03182ae24
Andrei Ranta <rantaandrei12@gmail.com>
2022-09-14 21:05:57 +0300

    #

--- www/db/config.js
+++ www/db/config.js
@@ -10,8 +10,7 @@ import "https://www.gstatic.com/firebasejs/8.0.0/firebase-database.js";
 const firebaseConfig = {
     apiKey: "AIzaSyCC1TTaWpCU4bxhBD2l7ueviKKP1eskrpM",
     authDomain: "medlog-9650b.firebaseapp.com",
-    databaseURL:
-        "https://medlog-9650b-default-rtdb.europe-west1.firebasedatabase.app",
+    databaseURL: "https://medlog-9650b-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "medlog-9650b",
     storageBucket: "medlog-9650b.appspot.com",
     messagingSenderId: "955776050336",
