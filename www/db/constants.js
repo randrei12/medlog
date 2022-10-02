60e14f7bef2d27f9aa7de457e156925b45d0c66b
David Ghinea <david.ghinea39@gmail.com>
2022-10-02 19:10:15 +0300

    tips

--- www/db/constants.js
+++ www/db/constants.js
@@ -9,7 +9,6 @@ const GENDER = {
 }
 
 const tips = [
-<<<<<<< HEAD
     'Adoptați o dietă sănătoasă', 
     'Evitati excesul de zahăr, sare și grăsime',
     'Evitați consumul excesiv de alcool', 
@@ -23,28 +22,6 @@ const tips = [
     'Vorbește cu cineva în care ai încredere dacă te simți deprimat', 
     'Luați antibiotice numai la prescripția medicului', 
     'Spălați-vă mâinile în mod corespunzător'
-=======
-    'Adoptați o dietă sănătoasă',
-    'Consumati mai putina sare si zahar',
-    'Reduceți aportul de grăsimi dăunătoare',
-    'Evitați utilizarea dăunătoare a alcoolului',
-    'Nu fumați',
-    'Adoptați un stil de viață activ',
-    'Verificați regulat tensiunea arterială',
-    'Vaccinul salvează vieți',
-    'Testați-vă împotriva bolilor dăunătoare',
-    'Acoperiți-vă gura când tușiți sau strănutați',
-    'Preveniți mușcăturile de țânțari',
-    'Respectați legile rutiere',
-    'Consumați numai apă sigură',
-    'Alăptați bebelușii de la 0 la 2 ani și mai mult',
-    'Vorbește cu cineva în care ai încredere dacă te simți deprimat',
-    'Luați antibiotice numai așa cum este prescris',
-    'Spălați-vă mâinile în mod corespunzător',
-    'Pregătiți-vă mâncarea corect',
-    'Faceți controale regulate',
-    'Odihniți-vă opt ore pe zi'
->>>>>>> bf9f2d2e6bb85eb863da7539a72e66b21e20ae94
 ]
 
 export { USER_TYPES, GENDER, tips};
