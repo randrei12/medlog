7e39a770fd79d22e09d8cc67bfe1b0da75f206f9
David Ghinea <david.ghinea39@gmail.com>
2022-10-02 19:02:46 +0300

    tips

--- www/db/constants.js
+++ www/db/constants.js
@@ -9,26 +9,19 @@ const GENDER = {
 }
 
 const tips = [
-    'Adoptați o dietă sănătoasă',
-    'Consumati mai putina sare si zahar',
-    'Reduceți aportul de grăsimi dăunătoare',
-    'Evitați utilizarea dăunătoare a alcoolului',
-    'Nu fumați',
-    'Adoptați un stil de viață activ',
-    'Verificați regulat tensiunea arterială',
-    'Vaccinați-vă',
-    'Testați-vă împotriva bolilor dăunătoare',
-    'Acoperiți-vă gura când tușiți sau strănutați',
-    'Preveniți mușcăturile de țânțari',
-    'Respectați legile rutiere',
-    'Consumați numai apă sigură',
-    'Alăptați bebelușii de la 0 la 2 ani și mai mult',
-    'Vorbește cu cineva în care ai încredere dacă te simți deprimat',
-    'Luați antibiotice numai așa cum este prescris',
-    'Spălați-vă mâinile în mod corespunzător',
-    'Pregătiți-vă mâncarea corect',
-    'Faceți controale regulate',
-    'Odihniți-vă opt ore pe zi'
+    'Adoptați o dietă sănătoasă', 
+    'Evitati excesul de zahăr, sare și grăsime',
+    'Evitați consumul excesiv de alcool', 
+    'Fumatul dăunează grav sănătății', 
+    'Adoptați un stil de viață activ', 
+    'Verificați-vă regulat tensiunea arterială', 
+    'Vaccinați-vă', 
+    'Testați-vă împotriva bolilor virale', 
+    'Acoperiți-vă gura și nasul atunci când tușiți sau strănutați', 
+    'Consumați apa numai din surse sigure', 
+    'Vorbește cu cineva în care ai încredere dacă te simți deprimat', 
+    'Luați antibiotice numai la prescripția medicului', 
+    'Spălați-vă mâinile în mod corespunzător'
 ]
 
 export { USER_TYPES, GENDER, tips};
