1ffd74c60050f9e2cdb1cc6435a56f548d79cbe9
Andrei Ranta <rantaandrei12@gmail.com>
2022-10-02 17:52:04 +0300

    city on doc account fix

--- www/js/doctor/account.js
+++ www/js/doctor/account.js
@@ -1,7 +1,32 @@
-if (!sessionStorage.getItem('user')) location = 'home.html';
-let data = JSON.parse(sessionStorage.getItem('user'));
+import "../../db/config.js";
+import { getLoggedUser, localities } from "../Utils.js";
+const locals = JSON.parse(localities);
 
-const [ backButton, signOutButton ] = document.querySelectorAll('.topNavContainer img')
+const [ backButton, signOutButton ] = document.querySelectorAll('.topNavContainer img');
+const [ firstName, lastName] = document.querySelectorAll('.docName input');
+const specialization = document.querySelector('.specialisation input');
+const [ CountySelect, CitySelect ] = document.querySelectorAll('.nameDiv > select');
+const phoneNumber = document.querySelector('.phoneNumber input');
+const email = document.querySelector('.email input');
+const hospital = document.querySelector('.hospital input');
+const aboutDoc = document.querySelector('.aboutDoc');
+const editButton = document.querySelector('.editButtonDocDiv > button');
+
+(async() => {
+    const user = await getLoggedUser();
+    if (!user) location = '../../index.html';
+    firstName.value = user.firstName;
+    lastName.value = user.lastName;
+    specialization.value = user.specialization;
+    CountySelect.value = user.county;
+    CountySelect.onchange = () => CitySelect.innerHTML = locals[CountySelect.value];
+    CitySelect.innerHTML = locals[CountySelect.value];
+    CitySelect.value = user.city;
+    phoneNumber.value = user.phoneNumber;
+    email.value = user.email;
+    hospital.value = user.hospital;
+    aboutDoc.value = user.description;
+})();
 
 backButton.onclick = () => history.back();
 signOutButton.onclick = () => {
@@ -10,28 +35,6 @@ signOutButton.onclick = () => {
     location = '../../index.html'
 }
 
-const [ firstName, lastName] = document.querySelectorAll('.docName input');
-const specialization = document.querySelector('.specialisation input');
-const [ CountySelect, CitySelect ] = document.querySelectorAll('.nameDiv > select');
-const phoneNumber = document.querySelector('.phoneNumber input');
-const email = document.querySelector('.email input');
-const hospital = document.querySelector('.hospital input');
-const aboutDoc = document.querySelector('.aboutDoc');
-
-firebase.firestore().collection('users').doc(data.uid).get().then(res => {
-    let user = res.data();
-    firstName.value = user.firstName;
-    lastName.value = user.lastName;
-    specialization.value = user.specialization;
-    CountySelect.value = user.county;
-    CitySelect.value = user.city;
-    phoneNumber.value = user.phoneNumber;
-    email.value = user.email;
-    hospital.value = user.hospital;
-    aboutDoc.value = user.description;
-});
-
-const editButton = document.querySelector('.editButtonDocDiv > button');
 editButton.onclick = () => {
     firebase.firestore().collection('users').doc(data.uid).update({
         firstName: firstName.value,
@@ -46,6 +49,6 @@ editButton.onclick = () => {
     }).then(() => {
         location = 'home.html'
     }).catch(e => {
-        alert('Error: ' + e.code)
+        alert('An error occured')
     })
 }
